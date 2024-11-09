4e297af765d82c36457fa3b3bcea9baa031c2074
Yuntwo <e1101579@u.nus.edu>
2024-11-09 22:49:22 +0800

    Add some ensemble config

--- ensemble.py
+++ ensemble.py
@@ -2,24 +2,36 @@ import os
 
 from langchain_community.retrievers import BM25Retriever, TFIDFRetriever
 from langchain.retrievers import EnsembleRetriever
-from vector_store import create_vector_db, get_vector_db
+from vector_store import create_vector_db, get_vector_db, create_low_dimension_vector_db, get_low_dimension_vector_db
 from dotenv import load_dotenv
-from optimized_retriever.full_text_retriever import CustomTFIDFRetriever
+from optimized_retriever.full_text_retriever import KeywordTFIDFRetriever, TruncatedSVDTFIDFRetriever, PCATFIDFRetriever
 
 
 # Hybrid search for nus mods
 def ensemble_retriever_from_mods(mods, embeddings=None):
     vs = get_vector_db()
     # vs = create_vector_db(mods, embeddings)
+    # vs = create_low_dimension_vector_db(embeddings=embeddings, reduced_dim=1536)
+    # vs = get_low_dimension_vector_db()
     vs_retriever = vs.as_retriever()
 
     # full_text_retriever = BM25Retriever.from_texts([t.page_content for t in mods])
-    # full_text_retriever = TFIDFRetriever.from_texts([t.page_content for t in mods])
-    full_text_retriever = CustomTFIDFRetriever.from_texts([t.page_content for t in mods])
+    full_text_retriever = TFIDFRetriever.from_texts([t.page_content for t in mods])
+    # full_text_retriever = KeywordTFIDFRetriever.from_texts([t.page_content for t in mods])
+    # 37242
+    # full_text_retriever = TruncatedSVDTFIDFRetriever.from_texts([t.page_content for t in mods], n_components=37242)
+    # full_text_retriever = PCATFIDFRetriever.from_texts([t.page_content for t in mods], n_components=14439)
 
     ensemble_retriever = EnsembleRetriever(
         retrievers=[full_text_retriever, vs_retriever],
-        weights=[0.1, 0.9])
+        weights=[0.5, 0.5])
+
+    # ensemble_retriever = EnsembleRetriever(
+    #     retrievers=[vs_retriever],
+    #     weights=[1])
+    # ensemble_retriever = EnsembleRetriever(
+    #     retrievers=[full_text_retriever],
+    #     weights=[1])
 
     return ensemble_retriever
 
