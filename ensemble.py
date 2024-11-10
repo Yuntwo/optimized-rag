3edbc7dda0893a83fdb5faf889001565838d5855
Yuntwo <e1101579@u.nus.edu>
2024-11-10 18:59:24 +0800

    Store and load full-text model from file

--- ensemble.py
+++ ensemble.py
@@ -5,18 +5,21 @@ from langchain.retrievers import EnsembleRetriever
 from vector_store import create_vector_db, get_vector_db, create_low_dimension_vector_db, get_low_dimension_vector_db
 from dotenv import load_dotenv
 from optimized_retriever.full_text_retriever import KeywordTFIDFRetriever, TruncatedSVDTFIDFRetriever, PCATFIDFRetriever
+import joblib
 
 
 # Hybrid search for nus mods
 def ensemble_retriever_from_mods(mods, embeddings=None):
-    vs = get_vector_db()
-    # vs = create_vector_db(mods, embeddings)
+    # vs = get_vector_db()
+    vs = create_vector_db(mods, embeddings)
     # vs = create_low_dimension_vector_db(embeddings=embeddings, reduced_dim=1536)
     # vs = get_low_dimension_vector_db()
     vs_retriever = vs.as_retriever()
 
     # full_text_retriever = BM25Retriever.from_texts([t.page_content for t in mods])
-    full_text_retriever = TFIDFRetriever.from_texts([t.page_content for t in mods])
+    # full_text_retriever = TFIDFRetriever.from_texts([t.page_content for t in mods])
+    full_text_retriever = joblib.load('model/full_text_retriever.pkl')
+    # joblib.dump(full_text_retriever, 'model/full_text_retriever.pkl')
     # full_text_retriever = KeywordTFIDFRetriever.from_texts([t.page_content for t in mods])
     # 37242
     # full_text_retriever = TruncatedSVDTFIDFRetriever.from_texts([t.page_content for t in mods], n_components=37242)
